8f4ff9c8328389274650fe577c8bbb2b5e445420
Joshua Pearce <jpearce@scottlogic.com>
2021-04-08 15:36:05 +0100

    Initial Commit

---
+++ RosyTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RosyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            PostCompilation.PostCompiledClass.PrintCrap();
+        }
+    }
+}
---
+++ TestGenerator/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace TestGenerator
+{
+    [Generator]
+    public class TestGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var sourceBuilder = new StringBuilder();
+
+            sourceBuilder.Append($@"
+                using System;
+
+                namespace PostCompilation
+                {{
+                    class PostCompiledClass
+                    {{
+                        public static void PrintCrap()
+                        {{
+                            Console.WriteLine(""It Worked!"");
+                        }}
+                    }}
+                }}
+            ");
+
+            context.AddSource("test", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+
+        void ISourceGenerator.Initialize(GeneratorInitializationContext context)
+        {
+            // nothing to do
+        }
+    }
+}
